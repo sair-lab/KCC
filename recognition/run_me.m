fc41ed529e3d5519cccecd9cab1e5248afc2f852
Jeffsan <wang.chen@zoho.com>
2017-11-23 12:24:31 +0800

    fix run_me.m

--- recognition/run_me.m
+++ recognition/run_me.m
@@ -4,3 +4,13 @@ data_conversion; % convert database to compatible format
 activity_recognition; % run recognition demo and show results
 
 %% you may also run show_example after running data_conversion to visulize the signals.
+show_example
+
+%% Example to use KCC similarity
+lambda = 0.0015;
+sigma = 1;
+
+correlator = kcc_train(example1, lambda, sigma);
+
+similarity_12 = kcc(example2, correlator)
+similarity_13 = kcc(example3, correlator)
