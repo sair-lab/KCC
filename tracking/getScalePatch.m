c659720c5623eddd3dee3dfa169991abaadc690a
JeffsanC <jeffsan.wang@gmail.com>
2017-11-23 12:50:03 +0800

    add header

--- tracking/getScalePatch.m
+++ tracking/getScalePatch.m
@@ -1,3 +1,21 @@
+%     KCC: Kernel Cross-Correlator
+%     Visual Tracking Using KCC
+%
+%     Copyright (C) 2017 
+%     Author: Wang Chen wang.chen@zoho.com Nanyang Technological University
+%             Zhang Le zhang.le@adsc.com Advanced Digital Sciences Center
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 function out = getScalePatch(img, pos, target_size, search_size, rotation, scale_model_sz, hog_scale_cell_size)
 
 % code from DSST
